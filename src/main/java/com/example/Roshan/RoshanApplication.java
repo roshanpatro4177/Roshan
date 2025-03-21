d6afab8d5ca65bc10ea283309a4bb90f929d98d0
roshanpatro4177 <roshanrpatro@gmail.com>
2025-03-21 19:47:36 +0530

    Initial commit

---
+++ src/main/java/com/example/Roshan/RoshanApplication.java
@@ -0,0 +1,13 @@
+package com.example.Roshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoshanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoshanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Roshan/RoshanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Roshan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoshanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
