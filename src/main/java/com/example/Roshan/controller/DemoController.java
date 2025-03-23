cbdd50c6de3285793c723cabcd662181edf3624e
Roshan Patro <87429310+roshanpatro4177@users.noreply.github.com>
2025-03-23 08:24:22 +0530

    Update DemoController.java

--- src/main/java/com/example/Roshan/controller/DemoController.java
+++ src/main/java/com/example/Roshan/controller/DemoController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    @GetMapping("/")
+    public String home() {
+        return "Webhook is running!";
+    }
+
     @GetMapping("/hello")
     public String helloWorld(){
         return "Welcome Roshan !!!";
